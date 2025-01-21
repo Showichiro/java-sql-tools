28cbbf736d0498273d65b5a5465b1d2f4210860d
uchihori <uchihori@primebrains.co.jp>
2025-01-21 11:30:22 +0900

    fix

--- src/main/java/jp/co/primebrains/sql_tools/command/subcommands/QueryCommand.java
+++ src/main/java/jp/co/primebrains/sql_tools/command/subcommands/QueryCommand.java
@@ -157,8 +157,10 @@ public class QueryCommand implements Callable<Integer> {
             switch (format) {
                 case csv:
                     exportToCsv(results, outputFile);
+                    break;
                 case excel:
                     exportToExcel(results, outputFile);
+                    break;
                 case yaml:
                     exportToYaml(results, outputFile);
                 default:
